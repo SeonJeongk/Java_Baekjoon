ac98164cbc2c327ffdc08d15df1b3a97449c80df
SeonJeongk <sjkim5621@naver.com>
2023-02-22 15:33:11 +0900

    N_2851 final

--- 2023_Java/src/N_2851.java
+++ 2023_Java/src/N_2851.java
@@ -9,28 +9,17 @@ public class N_2851 {
 			score[i] = input.nextInt();
 		}
 		
-		//diff(차이값)이 작아지다 다시 커지는 시점까지 계속 더한다
-		int sum = 0, diff = 1, j = 0;
-		while((diff > 0) && (j < 10)) {
-			sum += score[j];
-			System.out.println(sum);
-			diff = 100 - sum;
-			
-			if ((diff !=0) && (diff < 0)) {
-				sum -= score[j];
+		//100과의 차이값의 절대값은 더할수록 줄어든다 but 커진다면 100을 넘어선 것
+		//100을 넘은 버섯을 기준으로 이전값과 다음값의 합한 경우를 비교한다
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum += score[i];
+			//100에 가까운 최대값이 나왔을 때
+			if (Math.abs(sum - score[i] - 100) < sum - 100) {
+				sum -= score[i];
+				break;
 			}
-			j++;
 		}
-		if(j == 10) {	//모두 더해진 경우
-			System.out.println(sum);
-		}
-		else {
-			int checkNext = 100 - (sum + score[j]);	//다음수와 차이값
-			//100에 가까운 수가 2개일 경우 큰 값 선택
-			if(diff == checkNext) {
-				sum += score[j];
-			}
-			System.out.println(sum);
-		}
+		System.out.println(sum);
 	}
 }
